6b9791db68330dcea5101394eab90c3313e8d9e2
Feresey <p.milko1999@yandex.ru>
2020-08-13 21:01:31 +0300

    Fix all

--- lab3/lab3.lisp
+++ lab3/lab3.lisp
@@ -1,14 +1,12 @@
-(defun matrix-elem (i j matrix) (nth j (nth i matrix)))
-
 (defun max-in-lower-triangle (matrix)
   (let (
-      (len       (list-length matrix))
-      (result (matrix-elem 0 0 matrix))
+      (len    (first (array-dimensions matrix)))
+      (result (aref matrix 0 0))
     )
 
-    (loop for i from 0 upto (- len 1) do
+    (loop for i from 1 upto (- len 1) do
       (loop for j from 0 upto i do
-        (setf elem (matrix-elem i j matrix))
+        (setf elem (aref matrix i j))
         (if (> elem result) (setf result elem) 0)
       )
     )
--- lab3/main.lisp
+++ lab3/main.lisp
@@ -1,31 +1,34 @@
-; NIL
-(print (max-in-lower-triangle nil))
-(print (max-in-lower-triangle (list
-    '(1 0)
-    '(0 1)
+; 1
+(print (max-in-lower-triangle
+  #2A(
+    (1 0)
+    (0 1)
     )
   )
 )
 ; 1
-(print (max-in-lower-triangle (list
-    '(1 1)
-    '(1 1)
+(print (max-in-lower-triangle
+  #2A(
+    (1 1)
+    (1 1)
     )
   )
 )
 ; 9
-(print (max-in-lower-triangle (list
-    '(1 2 3)
-    '(4 5 6)
-    '(7 8 9)
+(print (max-in-lower-triangle
+  #2A(
+    (1 2 3)
+    (4 5 6)
+    (7 8 9)
     )
   )
 )
 ; 0
-(print (max-in-lower-triangle (list
-    (list 0 0 9999)
-    (list 0 0 9999)
-    (list 0 0 0)
+(print (max-in-lower-triangle
+  #2A(
+    (0 0 9999)
+    (0 0 9999)
+    (0 0 0)
     )
   )
 )
